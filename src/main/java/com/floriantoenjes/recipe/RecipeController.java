f00be2da0717a3f90d72c53e7297a095064d6b56
Florian <skatepainter@yahoo.de>
2016-08-28 13:39:42 +0200

    Major changes

--- src/main/java/com/floriantoenjes/recipe/RecipeController.java
+++ src/main/java/com/floriantoenjes/recipe/RecipeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
@@ -20,12 +21,20 @@ public class RecipeController {
         model.addAttribute("recipes", recipes);
         return "index";
     }
+    @RequestMapping(value = "/index", method = RequestMethod.POST)
+    public String addRecipe() {
+
+    }
 
     @RequestMapping("/recipe/{id}")
     public String add(@PathVariable Long id, Model model) {
         Recipe recipe = recipeService.findById(id);
         model.addAttribute("recipe", recipe);
         return "detail";
+    }
+    @RequestMapping("/add")
+    public String recipeForm() {
+        return null;
     }
 
 }

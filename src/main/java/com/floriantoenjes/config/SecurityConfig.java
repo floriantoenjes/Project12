a56d98f7155fb561269b53c5217e37ae8f7c9a1b
Florian <skatepainter@yahoo.de>
2016-09-06 17:19:16 +0200

    Implementing login

---
+++ src/main/java/com/floriantoenjes/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.floriantoenjes.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .anyRequest()
+                    .hasRole("USER")
+                    .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                    .successHandler(loginSuccessHandler())
+                    .failureHandler(loginFailureHandler())
+                    .and()
+                .logout()
+                    .permitAll()
+                    .logoutSuccessUrl("/login");
+    }
+
+    private AuthenticationFailureHandler loginFailureHandler() {
+        return (request, response, exception) -> {
+            response.sendRedirect("/login");
+        };
+    }
+
+    private AuthenticationSuccessHandler loginSuccessHandler() {
+        return ((request, response, authentication) -> response.sendRedirect("/"));
+    }
+}

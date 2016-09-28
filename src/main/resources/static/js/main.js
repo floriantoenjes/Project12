ae6bc0459d288ec94e198370e5e4cc04fd6382c7
Florian Tönjes <skatepainter@yahoo.de>
2016-09-28 17:59:11 +0200

    Added favorite listing on index page.

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -6,20 +6,20 @@ $("#add-ingredient").click(function(event) {
 function addIngredient() {
     var $ingredient = $(".ingredient-row:last");
     var inputIdName = $ingredient.children().first().children().first().attr("name");
-    var current = inputIdName.match("\\d");
+    var current = parseInt(inputIdName.match("\\d"));
     var n = current + 1;
 
     alert(n);
 
     $ingredient.after($('<div class="ingredient-row">' +
-            '<div class="prefix-20 grid-30">'
+            '<div class="prefix-20 grid-30">' +
                 '<input id="ingredients' + current + '.id"' + ' name="ingredients[' + current + '].id" type="hidden"/>' +
                 '<input id="ingredients' + current + '.version"' + ' name="ingredients[' + current + '].version" type="hidden"/>' +
                 '<p>' +
                     '<select id="ingredients' + current + '.item.name" name="ingredients[' + current + '].item.name">' +
 
                     '</select>' +
-                '/<p>' +
+                '</p>' +
             '</div>' +
         '</div>'
     ));

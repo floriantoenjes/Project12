770633937e727d259f54fa84a724410dcff11749
Florian Tönjes <skatepainter@yahoo.de>
2016-09-29 11:30:26 +0200

    Made adding of a new ingredient for editing work.

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -5,14 +5,24 @@ $("#add-ingredient").click(function(event) {
 
 function addIngredient() {
     var $lastIngredient = $(".ingredient-row:last");
+
+    // Get id of last ingredient-row
     var lastId = $lastIngredient.children().first().children().first().attr("id").match("\\d+")[0];
+
+    // Increment to new id
     var newId = (parseInt(lastId) + 1).toString();
+
     var $newIngredient = $lastIngredient.clone();
 
+    // Remove id and version hidden input nodes
+    $newIngredient.children().first().children().first().remove();
+    $newIngredient.children().first().children().first().remove();
+
+    // Replace id and name to be of the new id
     var regex1 = new RegExp("ingredients" + lastId, "g");
     var regex2 = new RegExp("\\[" + lastId + "\\]", "g");
-
     var newHtml = $newIngredient.html().replace(regex1, "ingredients" + newId).replace(regex2, "[" + newId + "]");
+
     $newIngredient.html(newHtml);
     $lastIngredient.after($newIngredient);
 }

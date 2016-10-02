00d2df1ee4ba291c85ebc92ebaba65387c523f72
Florian Tönjes <skatepainter@yahoo.de>
2016-10-02 10:43:12 +0200

    Added user validation.

--- src/main/java/com/floriantoenjes/user/UserController.java
+++ src/main/java/com/floriantoenjes/user/UserController.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 public class UserController {
@@ -14,12 +18,22 @@ public class UserController {
 
     @RequestMapping("/signup")
     public String signupForm(Model model) {
-        model.addAttribute("user", new User());
+        if (!model.containsAttribute("user")) {
+            model.addAttribute("user", new User());
+
+        }
         return "signup";
     }
 
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
-    public String signup(User user) {
+    public String signup(@Valid User user, BindingResult result, RedirectAttributes redirectAttributes) {
+
+        if (result.hasErrors()) {
+            redirectAttributes.addAttribute("org.springframework.validation.BindingResult.user", user);
+            redirectAttributes.addAttribute("user", user);
+            return "redirect:/signup";
+        }
+
         user.setRole(new Role("ROLE_USER"));
         userService.save(user);
         return "redirect:/index";
